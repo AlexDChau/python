eae0762b269d06eaeb22cf0bd37639104cdc8ce4
Xelark Chau <Xelark Chau>
2016-07-28 10:56:49 +0200

    'ChangeCode'

--- projet_python.py
+++ projet_python.py
@@ -1,4 +1,4 @@
-'Importation'
+'Import to use regex'
 import re
 
 class Matrix(object):
@@ -23,30 +23,44 @@ class Matrix(object):
         return self._matrix
 
     def fill_matrix(self):
-        'Méthode de remplissage de matrice'
+        'Methods to fill a matrix'
         for i in range(self._number_columns):
             values = input("Entrer "+str(self._number_rows)+" nombre séparé par des virgules: ")
             self._matrix[i] = values.split(",")
 
     def _sum_matrix(self, _mat):
-        if self._number_columns == _mat._number_columns:
-            for i in range(self._number_rows):
-                for j in range(self._number_columns):
-                    self._matrix[i][j] = int(self._matrix[i][j])+int(_mat._matrix[i][j])
-
-    def _substract_matrix(self, _mat):
-        if self._number_columns == _mat._number_columns:
-            for i in range(self._number_rows):
-                for j in range(self._number_columns):
-                    self._matrix[i][j] = int(self._matrix[i][j])-int(_mat._matrix[i][j])
-
-    def _multiply_matrix(self, _mat):
-        'Multiplication'
         if _mat is not None:
             if isinstance(_mat, Matrix):
-                print("ok")
-                print(str(self._number_columns) + " "+ str(_mat._get_number_rows))
-                if self._number_columns == _mat._get_number_rows:
+                # Each matrix MUST be the same dimension
+                if self._number_columns == _mat._number_columns\
+                and self._number_rows == _mat._number_rows:
+                    result = Matrix(self._number_columns, _mat._number_rows)
+                    for i in range(self._number_rows):
+                        for j in range(self._number_columns):
+                            result._matrix[i][j] = int(self._matrix[i][j]) + int(_mat._matrix[i][j])
+                    return result
+        return None
+
+    def _substract_matrix(self, _mat):
+        if _mat is not None:
+            if isinstance(_mat, Matrix):
+                # Each matrix MUST be the same dimension
+                if self._number_columns == _mat._number_columns\
+                and self._number_rows == _mat._number_rows:
+                    result = Matrix(self._number_columns, _mat._number_rows)
+                    for i in range(self._number_rows):
+                        for j in range(self._number_columns):
+                            result._matrix[i][j] = int(self._matrix[i][j]) - int(_mat._matrix[i][j])
+                    return result
+        return None
+
+    def _multiply_matrix(self, _mat):
+        'Method to multiply matrixes'
+        if _mat is not None:
+            if isinstance(_mat, Matrix):
+                # The number of columns of a matrix A MUST be
+                # equals to the number of rows of the matrix B
+                if self._number_columns == _mat._number_rows:
                     result = Matrix(self._number_columns, _mat._number_rows)
                     row = []
                     for i in range(self._number_columns):
@@ -75,13 +89,13 @@ class Matrix(object):
     def _load_matrix(self, filename):
         file = open(filename+'.matrix')
         for line in file:
-            row_pattern = r"(\[((-?\d,)*-?\d)\])"
+            row_pattern = r"(\[((-*\d+,)*-*\d+)\])"
             row_string = re.compile(r''+row_pattern).findall(line)
             count_row = 0
             invalide_number_rows = 0
             row_content = []
-            number_columns_of_file = int(row_string[0][1])
-            number_rows_of_file = int(row_string[1][1])
+            number_rows_of_file = int(row_string[0][1])
+            number_columns_of_file = int(row_string[1][1])
             for i in range(2, len(row_string)):
                 row_content = row_string[i][1].split(',')
                 j = 0
@@ -92,22 +106,36 @@ class Matrix(object):
                     invalide_number_rows = 1
                     break
                 count_row += 1
-
             if count_row == number_rows_of_file and invalide_number_rows == 0\
-            and number_columns_of_file == self._number_columns\
-            and number_rows_of_file == self._number_rows:
+            and number_columns_of_file == self._number_columns and\
+            number_rows_of_file == self._number_rows:
                 if re.compile(r'\[(\d)]\[(\d)]={'+row_pattern+'*}').match(line):
                     print("Le fichier est valide")
                 else:
                     print("Le fichier est invalide")
 
     def display(self):
-        'Affichage'
-        for row in self._matrix:
+        'Method to display a matrix'
+        max_length_per_column = []
+        for index_long in range(self._number_rows):
+            max_length_per_column.append(self._longest_of_the_column(index_long))
+        for i in range(self._number_columns):
             row_content = ""
-            for column in row:
-                row_content += str(column)+"\t"
+            for j in range(self._number_rows):
+                row_content += str(self._matrix[i][j])
+                k = 0
+                for space in range(max_length_per_column[j] - len(str(self._matrix[i][j])) + 1):
+                    row_content += " "
             print(row_content)
+
+    def _longest_of_the_column(self, row_index):
+        longest = 0
+        if row_index >= self._number_rows:
+            return -1
+        for i in range(self._number_columns):
+            if len(str(self._matrix[i][row_index])) > longest:
+                longest = len(str(self._matrix[i][row_index]))
+        return longest
 
     def __init__(self, number_columns, number_rows):
         self._number_columns = number_columns
